6c26336c3277925f274008f41e367cacc33eb4dc
AdarshIntellibottech <adarsh.naik@intellibottech.com>
2022-01-13 17:26:09 +0530

    latest commit

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,7 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Testing purpose from github')
+        'body': json.dumps('Testing purpose from github by git bash')
+        
+        'body': json.dumps('Testing purpose from github by git bash')
     }
