3b7d6455ab13f9eb917aeaf6e0ecd9e7eac6460c
AdarshIntellibottech <adarsh.naik@intellibottech.com>
2022-01-13 17:32:14 +0530

    latest commit

--- lambda_function.py
+++ lambda_function.py
@@ -6,5 +6,5 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Testing purpose from gitbash to aws lambda console')
         
-        'body': json.dumps('line for testing purpose')
+        'body': json.dumps('demo from editor')
     }
